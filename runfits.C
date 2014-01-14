10a71b77371c70908d25a19c67515f6d41b49771
acarvalh <alexandra.oliveira@cern.ch>
2014-01-14 17:36:24 +0100

    macro to run the R2G...mtot_range.cc code

---
+++ runfits.C
@@ -0,0 +1,18 @@
+{
+  gROOT->LoadMacro("R2GGBBFitter_mtot_range.cc");
+  runfits(400,1);
+}
+
+/*  TString incpath = gSystem->GetIncludePath();
+  incpath.Append(" -I/afs/cern.ch/cms/slc5_amd64_gcc472/lcg/roofit/5.34.04-cms2/include");
+  incpath.Append(" -L/afs/cern.ch/cms/slc5_amd64_gcc472/lcg/roofit/5.34.04-cms2/lib");
+  gSystem->SetIncludePath(incpath.Data());
+  gSystem->Load("libRooFitCore") ;
+  gSystem->Load("libRooStats") ;
+  using namespace RooFit;
+  using namespace RooStats ;
+  gStyle->SetStripDecimals(kFALSE);
+  gROOT->ProcessLine(".L GaussExp.cxx+");
+  //gROOT->LoadMacro("/afs/cern.ch/work/a/acarvalh/CMSSW_5_3_7/ggfits/R2GGBBFitter_mtot_side.cc");
+*/
+
