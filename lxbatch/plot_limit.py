6f77b52c4de5630d350e5ac5c8c5c214715da4b2
Badder <badder.marzocchi@cern.ch>
2014-07-17 11:28:49 +0200

    draw limits

---
+++ lxbatch/plot_limit.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+import string, sys, os, getopt, subprocess, time, shutil
+from math import log10, pow
+import numpy
+# ROOT setup
+import ROOT
+from ROOT import TFile, TTree, TLine, TChain, TCanvas, TH1D, TH2F, TLatex, TLegend, TStyle, gStyle, TMultiGraph, TGraph, TGraphErrors
+import math as m
+
+def usage():
+    print "Usage: plot_limit.py --mass=[mass] --min=[mjjmin] --max=[mjjmax] --step=[step] --nsteps=[nsteps]"
+    
+try:
+     opts, args = getopt.getopt(sys.argv[1:], "m:d:r:g:so:", ["mass=","min=","max=","step=","nsteps="])
+
+except getopt.GetoptError:
+     #* print help information and exit:*
+     usage()
+     sys.exit(2)
+
+mass = ''
+mjjmin = ''
+mjjmax = ''
+
+for opt, arg in opts:
+    
+     if opt in ("--mass"):
+        mass = arg
+     if opt in ("--min"):
+        mjjmin= arg     
+     if opt in ("--max"):
+        mjjmax= arg       
+     if opt in ("--step"):
+        step= arg    
+     if opt in ("--nsteps"):
+        nsteps= arg    
+
+print "Mass    = ",mass
+print "Min     = ",mjjmin 
+print "Max     = ",mjjmax
+print "Step    = ",step
+print "nSteps  = ",nsteps
+ 
+file = "limits.txt"
+name  = "cut_Mjj_study_reweight_m" + mass # + "_zoom"
+title = "Expected Limits at mass = " + mass + " GeV in both category. Use of all bkg samples"
+XaxisTittle = "Mjj_Min (GeV)"
+YaxisTittle = "Mjj_Max (GeV)"
+#ZaxisTittle = "Expected Limit"
+
+c1 = TCanvas()
+
+x = []
+y = []
+l_50 =[]
+l_50_sort =[]
+
+with open(file) as data:
+    for line in data:
+        mass_, cut1_, cut2_, exp_, res_ = line.split()
+        if str(50.0) in exp_ :
+            l_50.append(float(res_))
+            l_50_sort.append(float(res_))
+            x.append(float(cut1_))
+            y.append(float(cut2_))
+
+h2 = TH2F("h2",title,int(nsteps),float(mjjmin)-float(step)*(int(nsteps)-1)-float(step)/2,float(mjjmin)+float(step)/2,int(nsteps),float(mjjmax)-float(step)/2,float(mjjmax)+float(step)*(int(nsteps)-1)+float(step)/2)
+h2.SetStats(0)
+
+x = numpy.asarray(x, dtype='float')
+y = numpy.asarray(y, dtype='float')
+l_50 = numpy.asarray(l_50, dtype='float')
+l_50_sort = numpy.asarray(l_50_sort, dtype='float')
+l_50_sort.sort()
+
+norm=l_50_sort[0]
+l_50 = [i/norm for i in l_50]
+
+for i in xrange(int(nsteps)*int(nsteps)) :
+   print i," - ",x[i]," - ",y[i]," - ",l_50[i]
+   if l_50[i] > 3.: 
+      continue   
+   h2.SetBinContent(h2.FindBin(x[i],y[i]),l_50[i])
+
+
+h2.GetXaxis().SetTitle(XaxisTittle)
+h2.GetYaxis().SetTitle(YaxisTittle)
+#h2.GetZaxis().SetTitle(ZaxisTittle)
+h2.GetZaxis().SetRangeUser(0.95,1.05)
+h2.Draw("colz")
+
+c1.Update()
+
+c1.Print(name + ".pdf");
+c1.Print(name + ".gif");
+c1.Print(name + ".root");
+
+
---
+++ lxbatch/plot_limit.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+usage='Usage: -mass <inputMass> -min <inputMin> -max <inpuMax> -step <inputStep> -nsteps <inputNSteps>'
+
+
+
+args=`getopt rd: -- "$@"`
+if test $? != 0
+     then
+         echo $usage
+         exit 1
+fi
+
+
+
+eval set -- "$args"
+for i
+  do
+  case "$i" in
+      -mass) shift; inputMass=$2;shift;;
+      -min) shift; inputMin=$2;shift;;
+      -max) shift; inputMax=$2;shift;;
+      -step) shift; inputStep=$2;shift;;
+      -nsteps) shift; inputNSteps=$2;shift;;
+  esac
+done
+
+if [ "X"${inputMass} == "X" ]
+    then
+    echo
+echo "MISSING INPUT MASS! Please give a valid one!"
+    echo $usage
+    exit
+fi
+
+if [ "X"${inputMin} == "X" ]
+    then
+    echo
+echo "MISSING INPUT MINIMUM! Please give a valid one!"
+    echo $usage
+    exit
+fi
+
+if [ "X"${inputMax} == "X" ]
+    then
+    echo
+echo "MISSING INPUT MAXIMUM! Please give a valid one!"
+    echo $usage
+    exit
+fi
+
+if [ "X"${inputStep} == "X" ]
+    then
+    echo
+echo "MISSING INPUT STEP! Please give a valid one!"
+    echo $usage
+    exit
+fi
+
+if [ "X"${inputNSteps} == "X" ]
+    then
+    echo
+echo "MISSING INPUT NUMBER OF STEPS! Please give a valid one!"
+    echo $usage
+    exit
+fi
+
+rm limits.txt
+
+for((i = 0 ; i < ${inputNSteps} ; i++)); do 
+ for((j = 0 ; j < ${inputNSteps} ; j++)); do
+    mjj_min=$(($inputMin-$i*$inputStep));
+    mjj_max=$(($inputMax+$j*$inputStep));
+    echo "Mjj window = ["$mjj_min "," $mjj_max"]"
+    for file in `echo "limit_${inputMass}_${mjj_min}_${mjj_max}.txt"`; do
+        for exp in `echo "2.5 16.0 50.0 84.0 97.5"`; do
+            res=`cat ${file} | 'grep' "Expected" | 'grep' "${exp}%" | cut -d "<" -f 2 `
+	    echo "${inputMass} ${mjj_min} ${mjj_max} ${exp} ${res}" >> limits.txt
+        done
+    done
+ done
+done
+
