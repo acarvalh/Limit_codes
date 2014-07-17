b865bc5d31ebb52d2fb953097ce37cd8b9435f62
Badder <badder.marzocchi@cern.ch>
2014-07-17 11:36:20 +0200

    draw limits

--- runfits.C
+++ runfits.C
@@ -1,18 +1,6 @@
 {
-  gROOT->LoadMacro("R2GGBBFitter_mtot_range.cc");
-  runfits(500,1);
+  gROOT->LoadMacro("MACRO");
+  TString cuts_tmp = "";
+  TString baseDir = "";
+  runfits(MASS,1,false,cuts_tmp,baseDir);
 }
-
-/*  TString incpath = gSystem->GetIncludePath();
-  incpath.Append(" -I/afs/cern.ch/cms/slc5_amd64_gcc472/lcg/roofit/5.34.04-cms2/include");
-  incpath.Append(" -L/afs/cern.ch/cms/slc5_amd64_gcc472/lcg/roofit/5.34.04-cms2/lib");
-  gSystem->SetIncludePath(incpath.Data());
-  gSystem->Load("libRooFitCore") ;
-  gSystem->Load("libRooStats") ;
-  using namespace RooFit;
-  using namespace RooStats ;
-  gStyle->SetStripDecimals(kFALSE);
-  gROOT->ProcessLine(".L GaussExp.cxx+");
-  //gROOT->LoadMacro("/afs/cern.ch/work/a/acarvalh/CMSSW_5_3_7/ggfits/R2GGBBFitter_mtot_side.cc");
-*/
-
