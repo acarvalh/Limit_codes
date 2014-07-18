533e8953a08a52ddac8684ffe9e328565daec495
maxime <mgouzevi@cern.ch>
2014-07-18 12:24:49 +0200

    toto

--- R2GGBBFitter_mtot_range.cc
+++ R2GGBBFitter_mtot_range.cc
@@ -43,6 +43,7 @@ RooArgSet* defineVariables()
   // define variables of the input ntuple
   RooRealVar* mtot = new RooRealVar("mtot","M(#gamma#gamma jj)",320,1200,"GeV");
   RooRealVar* mgg = new RooRealVar("mgg","M(#gamma#gamma)",100,180,"GeV");
+  RooRealVar* mjj = new RooRealVar("mjj","M(jj)",0,500,"GeV");
   RooRealVar* evWeight = new RooRealVar("evWeight","HqT x PUwei",0,100000000,"");
   RooCategory* cut_based_ct = new RooCategory("cut_based_ct","event category 2") ;
   //
@@ -53,6 +54,7 @@ RooArgSet* defineVariables()
   ntplVars->add(*cut_based_ct);
   ntplVars->add(*mtot);
   ntplVars->add(*mgg);
+  ntplVars->add(*mjj);
   ntplVars->add(*evWeight);
   return ntplVars;
 }
@@ -70,11 +72,15 @@ void runfits(const Float_t mass=120, Int_t mode=1, Bool_t dobands = false)
   RooFitResult* fitresults;
 // TString ssignal = "MiniTrees/OlivierAug13/v02_regkin_mggjj_0/Radion_m500_regression-m500_minimal.root";
 // TString ddata = "MiniTrees/OlivierAug13/v02_regkin_mggjj_0/Data_regression-m500_minimal.root";
-// TString ssignal = "MiniTrees/OlivierOc13/v15_base_mggjj_0/02013-10-30-Radion_m1100_8TeV_nm_m1100.root";
-// TString ddata = "MiniTrees/OlivierOc13/v15_base_mggjj_0/02013-10-30-Data_m1100.root";
+// TString ssignal = "MiniTrees/OlivierOc13/v15_base_mggjj_0/02013-10-30-Radion_m1100_8TeV_nm_m500.root";
+// TString ddata = "MiniTrees/OlivierOc13/v15_base_mggjj_0/02013-10-30-Data_m500.root";
   //
+  ////  TString ssignal = "/afs/cern.ch/user/f/fajimene/CMSSW_6_1_1/src/Limit_codes/LimitTrees/v33_fitToMggjj_withKinFit/Radion_m500_8TeV_m500.root";
+  //// TString ddata = "/afs/cern.ch/user/f/fajimene/CMSSW_6_1_1/src/Limit_codes/LimitTrees/v33_fitToMggjj_withKinFit/DataCS_m500.root";
+
   TString ssignal = "/afs/cern.ch/work/o/obondu/public/forRadion/limitTrees/v28/v28_fitToMggjj_withKinFit/Radion_m500_8TeV_m500.root";
   TString ddata = "/afs/cern.ch/work/o/obondu/public/forRadion/limitTrees/v28/v28_fitToMggjj_withKinFit/Data_m500.root";
+
   //
   cout<<"Signal: "<< ssignal<<endl;
   cout<<"Data: "<< ddata<<endl;
@@ -127,7 +133,7 @@ void AddSigData(RooWorkspace* w, Float_t mass, TString signalfile) {
 //  RooDataSet *sigScaled = new RooDataSet("sigScaled", "dataset",sigTree, *ntplVars1,"", "nw");
   //
   RooDataSet* sigToFit[ncat];
-  TString cut0 = "&& mgg > 120 && mgg < 130 "; // " && 1>0";//
+  TString cut0 = "&& mgg > 120 && mgg < 130 && mjj > 90 && mjj < 165 "; // " && 1>0";//
   for (int c = 0; c < ncat; ++c) {
     sigToFit[c] = (RooDataSet*) sigScaled.reduce(
         *w->var("mtot"),
@@ -167,7 +173,7 @@ void AddBkgData(RooWorkspace* w, TString datafile) {
 
   RooDataSet* dataToFit[ncat];
   RooDataSet* dataToPlot[ncat];
-  TString cut0 = "&& mgg > 120 && mgg < 130 "; // " && 1>0";//
+  TString cut0 = "&& mgg > 120 && mgg < 130 && mjj > 90 && mjj < 165 "; // " && 1>0";//
   for (int c = 0; c < ncat; ++c) {
     if(c==0) dataToFit[c] = (RooDataSet*) Data.reduce(
         *w->var("mtot"),
@@ -500,8 +506,13 @@ void MakeBkgWS(RooWorkspace* w, const char* fileBaseName) {
   RooWorkspace *wAll = new RooWorkspace("w_all","w_all");
   for (int c = 0; c < ncat; ++c) {
     data[c] = (RooDataSet*) w->data(TString::Format("Data_cat%d",c));
+    RooDataHist* dataBinned = data[c]->binnedClone();
+
     mtotBkgPdf[c] = (RooAbsPdf*) w->pdf(TString::Format("mtotBkg_cat%d",c));
     wAll->import(*data[c], Rename(TString::Format("data_obs_cat%d",c)));
+    
+    //comment this out if you want to use weighted data wAll->import(*dataBinned, Rename(TString::Format("data_obs_cat%d",c)));
+
     wAll->import(*w->pdf(TString::Format("mtotBkg_cat%d",c)));
     wAll->factory(
         TString::Format("CMS_hgg_bkg_8TeV_cat%d_norm[%g,0.0,100000.0]",
