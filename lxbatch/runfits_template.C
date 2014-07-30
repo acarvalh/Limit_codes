da65c8af17b40ff46428953c8618120fcc3ffe71
Badder <badder.marzocchi@cern.ch>
2014-07-30 15:39:03 +0200

    bugfix

--- lxbatch/runfits_template.C
+++ lxbatch/runfits_template.C
@@ -1,6 +1,7 @@
 {
   gROOT->LoadMacro("MACRO");
-  TString cuts_tmp = " && VAR > MIN && VAR < MAX";
+  TString cuts_tmp0 = " && VAR > MIN && VAR < MAX";
+  TString cuts_tmp1 = " && VAR > MIN && VAR < MAX";
   TString baseDir = "BASEDIR../";
-  runfits(MASS,1,false,cuts_tmp,baseDir);
+  runfits(MASS,1,false,cuts_tmp0,cuts_tmp1,baseDir);
 }
