42eb7ce93b784337494ebe8a37e3443ba9ea1c34
Badder <badder.marzocchi@cern.ch>
2014-10-28 12:35:19 +0100

    papar_limit

--- R2GGBBFitter_mgg_addhiggs.cc
+++ R2GGBBFitter_mgg_addhiggs.cc
@@ -74,22 +74,22 @@ void runfits(const Float_t mass=120, Int_t mode=1, Bool_t dobands = false)
   bool cutbased=true;
   // the minitree to be addeed
   //
-  TString hhiggsggh = "/afs/cern.ch/work/o/obondu/public/forRadion/limitTrees/v33/v33_fitToMgg_noKinFit/ggh_m125_powheg_8TeV_m350.root";
-  TString hhiggstth = "/afs/cern.ch/work/o/obondu/public/forRadion/limitTrees/v33/v33_fitToMgg_noKinFit/tth_m125_8TeV_m350.root";
-  TString hhiggsvbf = "/afs/cern.ch/work/o/obondu/public/forRadion/limitTrees/v33/v33_fitToMgg_noKinFit/vbf_m125_8TeV_m350.root";
-  TString hhiggsvh = "/afs/cern.ch/work/o/obondu/public/forRadion/limitTrees/v33/v33_fitToMgg_noKinFit/wzh_m125_8TeV_zh_m350.root";
-  TString hhiggsbbh = "/afs/cern.ch/work/o/obondu/public/forRadion/limitTrees/v33/v33_fitToMgg_noKinFit/bbh_m125_8TeV_m350.root";
+  TString hhiggsggh = "/afs/cern.ch/user/h/hebda/public/forRadion/v35_fitToMgg_resSearch_withKinFit/ggh_m125_powheg_8TeV_m300.root";
+  TString hhiggstth = "/afs/cern.ch/user/h/hebda/public/forRadion/v35_fitToMgg_resSearch_withKinFit/tth_m125_8TeV_m300.root";
+  TString hhiggsvbf = "/afs/cern.ch/user/h/hebda/public/forRadion/v35_fitToMgg_resSearch_withKinFit/vbf_m125_8TeV_m300.root";
+  TString hhiggsvh = "/afs/cern.ch/user/h/hebda/public/forRadion/v35_fitToMgg_resSearch_withKinFit/wzh_m125_8TeV_zh_m300.root";
+  TString hhiggsbbh = "/afs/cern.ch/user/h/hebda/public/forRadion/v35_fitToMgg_resSearch_withKinFit/bbh_m125_8TeV_m300.root";
   //
-  TString ssignal = "/afs/cern.ch/work/o/obondu/public/forRadion/limitTrees/v33/v33_fitToMgg_noKinFit/MSSM_m350_8TeV_m350.root ";
-  TString ddata = "/afs/cern.ch/work/o/obondu/public/forRadion/limitTrees/v33/v33_fitToMgg_noKinFit/Data_m350.root";
+  TString ssignal = "/afs/cern.ch/user/h/hebda/public/forRadion/v35_fitToMgg_resSearch_withKinFit/Radion_m300_8TeV_m300.root ";
+  TString ddata = "/afs/cern.ch/user/h/hebda/public/forRadion/v35_fitToMgg_resSearch_withKinFit/Data_m300.root";
   //
-  // TString hhiggs = "MiniTrees/OlivierOc13/v16_base_mgg_0_massCutVersion0/02013-11-05-Radion_m350_8TeV_nm_m350.root";
-  // TString ssignal = "MiniTrees/OlivierOc13/v16_base_mgg_0_massCutVersion0/02013-11-05-Radion_m350_8TeV_nm_m350.root";
-  // TString ddata = "MiniTrees/OlivierOc13/v16_base_mgg_0_massCutVersion0/02013-11-05-Data_m350.root";
+  // TString hhiggs = "MiniTrees/OlivierOc13/v16_base_mgg_0_massCutVersion0/02013-11-05-Radion_m350_8TeV_nm_m300.root";
+  // TString ssignal = "MiniTrees/OlivierOc13/v16_base_mgg_0_massCutVersion0/02013-11-05-Radion_m350_8TeV_nm_m300.root";
+  // TString ddata = "MiniTrees/OlivierOc13/v16_base_mgg_0_massCutVersion0/02013-11-05-Data_m300.root";
   //
-  // TString hhiggs = "MiniTrees/OlivierOc13/v15_regkin_mgg_0_massCutVersion0/02013-10-30-Radion_m350_8TeV_nm_m350.root";
-  // TString ssignal = "MiniTrees/OlivierOc13/v15_regkin_mgg_0_massCutVersion0/02013-10-30-Radion_m350_8TeV_nm_m350.root";
-  // TString ddata = "MiniTrees/OlivierOc13/v15_regkin_mgg_0_massCutVersion0/02013-10-30-Data_m350.root";
+  // TString hhiggs = "MiniTrees/OlivierOc13/v15_regkin_mgg_0_massCutVersion0/02013-10-30-Radion_m350_8TeV_nm_m300.root";
+  // TString ssignal = "MiniTrees/OlivierOc13/v15_regkin_mgg_0_massCutVersion0/02013-10-30-Radion_m350_8TeV_nm_m300.root";
+  // TString ddata = "MiniTrees/OlivierOc13/v15_regkin_mgg_0_massCutVersion0/02013-10-30-Data_m300.root";
   //
   cout<<"Signal: "<<ssignal<<endl;
   cout<<"Data: "<<ddata<<endl;
@@ -614,7 +614,7 @@ RooFitResult* BkgModelFitBernstein(RooWorkspace* w, Bool_t dobands) {
     legmcH->AddEntry(plotmggBkg[c]->getObject(7),"VBF ","LPE"); // not...
     legmcH->AddEntry(plotmggBkg[c]->getObject(9),"VH ","LPE"); // not...
     legmcH->AddEntry(plotmggBkg[c]->getObject(11),"bbH ","LPE"); // not...
-    legmc->SetHeader(" 350 GeV");
+    legmc->SetHeader(" 300 GeV");
     legmcH->SetHeader(" Higgs");
     legmc->SetBorderSize(0);
     legmc->SetFillStyle(0);
@@ -870,7 +870,7 @@ void MakePlots(RooWorkspace* w, Float_t Mass) {
     // float effS = effSigma(hist);
     TLatex *lat = new TLatex(
 			     minSigFit+0.5,0.85*plotmgg[c]->GetMaximum(),
-			     " Resonance - 350 GeV");
+			     " Resonance - 300 GeV");
     lat->Draw();
     TLatex *lat2 = new TLatex(
 			      minSigFit+1.5,0.75*plotmgg[c]->GetMaximum(),catdesc.at(c));
@@ -989,7 +989,7 @@ void MakePlotsHiggs(RooWorkspace* w, Float_t Mass) {
       // float effS = effSigma(hist);
       TLatex *lat = new TLatex(
 			       minSigFit+0.5,0.85*plotmgg[c]->GetMaximum(),
-			       " Resonance - 350 GeV");
+			       " Resonance - 300 GeV");
       lat->Draw();
       TLatex *lat2 = new TLatex(
 				minSigFit+1.5,0.75*plotmgg[c]->GetMaximum(),catdesc.at(c));
@@ -1225,7 +1225,7 @@ void MakeDataCard(RooWorkspace* w, const char* fileBaseName, const char* fileBkg
   outFile << "#Run with: combine -d hgg.mH350.0.shapes-Unbinned.txt -U -m 130 -H ProfileLikelihood -M MarkovChainMC --rMin=0 --rMax=20.0 -b 3500 -i 50000 --optimizeSim=1 --tries 30" << endl;
   outFile << "# Lumi = " << "19785" << " pb-1" << endl;
   outFile << "imax "<<ncat << endl;
-  outFile << "jmax 5" << endl; // number of BKG
+  outFile << "jmax 6" << endl; // number of BKG
   outFile << "kmax *" << endl;
   outFile << "---------------" << endl;
   outFile << "shapes data_obs cat0 " << TString(fileBkgName)+".root" << " w_all:data_obs_cat0" << endl;
@@ -1294,8 +1294,8 @@ void MakeDataCard(RooWorkspace* w, const char* fileBaseName, const char* fileBkg
 	    << "0.988 - 0.988 0.988 0.988 0.988 0.988 "
 	    <<"# b tag efficiency uncertainty" << endl;
     outFile << "maajj_cut_acceptance lnN "
-	    << "1.02 - 1.02 1.02 1.02 1.02 1.02"
-	    << "1.02 - 1.02 1.02 1.02 1.02 1.02" << endl;
+	    << "1.02 - 1.02 1.02 1.02 1.02 1.02 "
+	    << "1.02 - 1.02 1.02 1.02 1.02 1.02 " << endl;
     outFile << " " << endl;
     outFile << "############## Theory uncertainties on SM Higgs production " << endl;
     outFile << "PDF lnN "
