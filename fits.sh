da4962a371e48c5883fb26ccb23d50b3425c89db
hebda <phil.hebda@gmail.com>
2014-11-08 01:02:19 +0100

    added running over multiple limit tree dirs, cleaned up, running over nonres/res and 1D/2D

--- fits.sh
+++ fits.sh
@@ -1,105 +1,101 @@
 #!/bin/bash
 
-#change windows for cuts
-declare -a radion=("260" "270" "300" "350" "400" "450" "500"  )
-declare -a winl0=("200" "250" "250" "300" "350" "400" "450" )
-declare -a winu0=("280" "290" "350" "400" "450" "500" "550" )
-#
-declare -a winl1=("200" "250" "250" "300" "350" "400" "450" )
-declare -a winu1=("280" "290" "350" "400" "450" "500" "550" )
-# R2GGBBFitter_mgg_addhiggs.cc
-# models_mtot_exp.rs
 doBlinding=1
 
-for (( i = 4 ; i < 5 ; i++ )); do # for each working point
-  mkdir radlim_CSV_WP$i
-#   for (( j = 0 ; j < ${#radion[@]} ; j++ )); do # for each mass
-#  for (( j = 1 ; j < 7; j++ )); do # for each mass
-#   for (( j = 0 ; j < 1 ; j++ )); do # for each mass
-   for (( j = 1 ; j < 2 ; j++ )); do # for each mass
-        # the signal file
-        sed -i -r -e "s/WP[0-9]/WP$i/g" R2GGBBFitter_mgg_addhiggs.cc
-        sed -i -r -e "s/m[0-9]+.root/m${radion[$j]}.root/g" R2GGBBFitter_mgg_addhiggs.cc 
-        sed -i -r -e "s/Radion_m[0-9]+_8TeV_m[0-9]+.root/Radion_m${radion[$j]}_8TeV_m${radion[$j]}.root/g" R2GGBBFitter_mgg_addhiggs.cc
-        # the ggH file
-        #sed -i -r -e "s/WP[0-9]/WP$i/g" R2GGBBFitter_mgg_addhigg_ggH.cc
-        #sed -i -r -e "s/m[0-9]+.root/m${radion[$j]}.root/g" R2GGBBFitter_mgg_addhigg_ggH.cc 
-        # the ttH file
-        #sed -i -r -e "s/WP[0-9]/WP$i/g" R2GGBBFitter_mgg_addhigg_ttH.cc
-        #sed -i -r -e "s/m[0-9]+.root/m${radion[$j]}.root/g" R2GGBBFitter_mgg_addhigg_ttH.cc 
-        # the signal file
-        #sed -i -r -e "s/WP[0-9]/WP$i/g" R2GGBBFitter_mgg_addhigg_vbf.cc
-        #sed -i -r -e "s/m[0-9]+.root/m${radion[$j]}.root/g" R2GGBBFitter_mgg_addhigg_vbf.cc 
-        # the signal file
-        #sed -i -r -e "s/WP[0-9]/WP$i/g" R2GGBBFitter_mgg_addhigg_vH.cc
-        #sed -i -r -e "s/m[0-9]+.root/m${radion[$j]}.root/g" R2GGBBFitter_mgg_addhigg_vH.cc 
+version="v37"
+basedir="/afs/cern.ch/work/o/obondu/public/forRadion/limitTrees/$version"
+limitdirs=("${version}_fitToMgg_nonresSearch_withKinFit" "${version}_fitToMgg_resSearch_withKinFit" "${version}_fitToMgg_resSearch_withRegKinFit" "${version}_fitTo2D_nonresSearch_withKinFit" "${version}_fitTo2D_resSearch_withRegKinFit" "${version}_fitTo2D_resSearch_withKinFit" "${version}_fitToFTR14001_nonresSearch_withKinFit")
+doResLimits=("0" "1" "1" "0" "1" "1" "0")
+do2DLimits=("0" "0" "0" "1" "1" "1" "1")
+mkdir radlim
+
+#If you only want to run on a subset of directories, edit this array with the appropriate indices.
+runLimts=("0" "1" "2" "3" "4" "5" "6")
+
+for i in `echo ${runLimits[@]}`; do
+
+    dir="$basedir/${limitdirs[$i]}"
+
+    if [ ${doResLimits[$i]} == "0" ]; then
+	masses=("0")
+    else
+	masses=("260" "270" "300" "350" "400" "450" "500")
+    fi
+
+    if [ ${do2DLimits[$i]} == "0" ]; then
+	fitterScript=R2GGBBFitter_mgg_addhiggs.cc
+    else
+	fitterScript=R2GGBBFitter_2D_addhiggs.cc
+    fi
+
+    for imass in `echo ${masses[@]}`; do
+
+	#Prepare the input files
+	sed -i "/TString hhiggsggh/c\TString hhiggsggh = \"$dir/${limitdirs[$i]}/ggh_m125_powheg_8TeV_m${imass}.root\"/" $fitterScript
+	sed -i "/TString hhiggstth/c\TString hhiggstth = \"$dir/${limitdirs[$i]}/tth_m125_8TeV_m${imass}.root\"/" $fitterScript
+	sed -i "/TString hhiggsvbf/c\TString hhiggsvbf = \"$dir/${limitdirs[$i]}/vbf_m125_8TeV_m${imass}.root\"/" $fitterScript
+	sed -i "/TString hhiggsvh/c\TString hhiggsvh = \"$dir/${limitdirs[$i]}/wzh_m125_8TeV_zh_m${imass}.root\"/" $fitterScript
+	sed -i "/TString hhiggsbbh/c\TString hhiggsbbh = \"$dir/${limitdirs[$i]}/bbh_m125_8TeV_m${imass}.root\"/" $fitterScript
+	sed -i "/TString ddata/c\TString ddata = \"$dir/${limitdirs[$i]}/Data_m${imass}.root\"/" $fitterScript
+
+	if [ ${imass} == "0" ]; then
+	    sed -i "/TString ssignal/c\TString ssignal = \"$dir/${limitdirs[$i]}/ggHH_8TeV_m0.root\"/" $fitterScript
+	elif [ ${imass} == "260" ]; then
+	    sed -i "/TString ssignal/c\TString ssignal = \"$dir/${limitdirs[$i]}/MSSM_m260_8TeV_m260.root\"/" $fitterScript
+	else
+	    sed -i "/TString ssignal/c\TString ssignal = \"$dir/${limitdirs[$i]}/Radion_m${imass}_8TeV_${imass}.root\"/" $fitterScript
+	fi
+
         # the legend
-        sed -i -r -e "s/[0-9]+ GeV\"\);/${radion[$j]} GeV\"\);/g" R2GGBBFitter_mgg_addhiggs.cc
-        # name of files
-        #sed -i -r -e "s/mH[0-9]+/mH${radion[$j]}/g" R2GGBBFitter_mgg_addhiggs.cc
-        #sed -i -r -e "s/m[0-9]+\_regression/${radion[$j]}\_regression/g" R2GGBBFitter_mgg_addhiggs.cc
-        #sed -i -r -e "s/m[0-9]+\_/m${radion[$j]}\_/g" R2GGBBFitter_mgg_addhiggs.cc
-        #sed -i -r -e "s/m[0-9]+\_/m${radion[$j]}\_/g" models_mtot_exp.rs
-        # the window mtot > 550 || mtot < 450
-        #sed -i -r -e "s/mtot > [0-9]+ \|\| mtot < [0-9]+/mtot > ${winu[$j]} \|\| mtot < ${winl[$j]}/g" R2GGBBFitter_mgg_addhiggs.cc
-        echo WP$i MR ${radion[$j]}
-        # the cuts
-        # TString cut0 = "&& mtot > 455 && mtot < 550 "; //"&& 1>0";//
-        # TString cut1 = "&& mtot > 455 && mtot < 550 "; // "&& 1>0";//
-        # TString cutj0 = "&& mjj > 90 && mjj < 170 "; //"&& 1>0";//
-        # TString cutj1 = "&& mjj > 100 && mjj < 160 "; // "&& 1>0";//
-        # the signal parameters (median to fit)
-#        sed -i -r -e "s/TString cut0 = \"\&\& mtot > [0-9]+ \&\& mtot < [0-9]+/TString cut0 = \"\&\& mtot > ${winl0[$j]} \&\& mtot < ${winu0[$j]}/g" R2GGBBFitter_mgg_addhiggs.cc
-#        sed -i -r -e "s/TString cut1 = \"\&\& mtot > [0-9]+ \&\& mtot < [0-9]+/TString cut1 = \"\&\& mtot > ${winl1[$j]} \&\& mtot < ${winu1[$j]}/g" R2GGBBFitter_mgg_addhiggs.cc
-        #
-        mkdir radlim_CSV_WP$i/radlim${radion[$j]}_CSV/
-        root -l -q runmgg.C >> radlim_CSV_WP$i/radlim${radion[$j]}_CSV/log_radlim${radion[$j]}.txt
-        # overwrite the higgs WS
-        #root -l -q runmgg_ggH.C >> radlim_CSV_WP$i/radlim${radion[$j]}_CSV/log_radlim${radion[$j]}_ggH.txt
-        #root -l -q runmgg_ttH.C >> radlim_CSV_WP$i/radlim${radion[$j]}_CSV/log_radlim${radion[$j]}_ttH.txt
-        #root -l -q runmgg_vbf.C >> radlim_CSV_WP$i/radlim${radion[$j]}_CSV/log_radlim${radion[$j]}_vbf.txt
-        #root -l -q runmgg_vH.C >> radlim_CSV_WP$i/radlim${radion[$j]}_CSV/log_radlim${radion[$j]}_vH.txt
-	mv workspaces/*.root radlim_CSV_WP$i/radlim${radion[$j]}_CSV
-	mv datacards/*.txt radlim_CSV_WP$i/radlim${radion[$j]}_CSV
+	sed -i -r -e "s/[0-9]+ GeV\"\);/${masses[$j]} GeV\"\);/g" $fitterScript
 
-        #also colect the plots
-        mv databkgoversig* radlim_CSV_WP$i/radlim${radion[$j]}_CSV/
-        mv sigmodel* radlim_CSV_WP$i/radlim${radion[$j]}_CSV/
-        mv higmodel* radlim_CSV_WP$i/radlim${radion[$j]}_CSV/
-        mv remenber.txt radlim${radion[$j]}_CSV
+	echo "Running limits for $imass on ${limitdirs[$i]}"
+
+	outputdir="radlim_${limitdirs[$i]}/radlim${imass}"
+	mkdir $outputdir
+	root -l -q runmgg.C >> ${outputdir}/log_radlim${imass}.txt
+	mv workspaces/*.root $outputdir
+	mv datacards/*.txt $outputdir
+
+       #also colect the plots
+	mv databkgoversig* $outputdir
+	mv sigmodel* $outputdir
+	mv higmodel* $outputdir
+
         ## create limits root files for each mass
-        cd radlim_CSV_WP$i/radlim${radion[$j]}_CSV/
-
-        if [ ${doBlinding} == 1 ]    
-        then
-                combine -M Asymptotic --run blind hgg.mH125.0_8TeV.txt >> higgsCombineTest.Asymptotic.mH125.6.mR${radion[$j]}_higgs.txt
-                mv higgsCombineTest.Asymptotic.mH120.root higgsCombineTest.Asymptotic.mH125.mR${radion[$j]}_higgs.root
-	        echo did with Higgs
+	cd $outputdir
+	
+	if [ ${doBlinding} == 1 ]    
+	then
+	    combine -M Asymptotic --run blind hgg.mH125.0_8TeV.txt >> higgsCombineTest.Asymptotic.mH125.6.mR${imass}_higgs.txt
+	    mv higgsCombineTest.Asymptotic.mH120.root higgsCombineTest.Asymptotic.mH125.mR${imass}_higgs.root
+	    echo did with Higgs
 	        
-                combine -M Asymptotic --run blind hgg.mH125.0_8TeVonecatnohiggs.txt >> higgsCombineTest.Asymptotic.mH125.6.mR${radion[$j]}_onecatnohiggs.txt
-                mv higgsCombineTest.Asymptotic.mH120.root higgsCombineTest.Asymptotic.mH125.mR${radion[$j]}_onecatnohiggs.root
-	        echo did one categ no higgs
+	    combine -M Asymptotic --run blind hgg.mH125.0_8TeVonecatnohiggs.txt >> higgsCombineTest.Asymptotic.mH125.6.mR${imass}_onecatnohiggs.txt
+	    mv higgsCombineTest.Asymptotic.mH120.root higgsCombineTest.Asymptotic.mH125.mR${imass}_onecatnohiggs.root
+	    echo did one categ no higgs
 
-                combine -M Asymptotic --run blind hgg.mH125.0_8TeV.txt -S 0 >> higgsCombineTest.Asymptotic.mH125.6.mR${radion[$j]}_nosyst_higgs.txt
-                mv higgsCombineTest.Asymptotic.mH120.root higgsCombineTest.Asymptotic.mH125.mR${radion[$j]}_nosyst_higgs.root
-	        echo did with no syst with higgs
-                rm roostats*
-                cd ../..
-        else
-                combine -M Asymptotic hgg.mH125.0_8TeV.txt >> higgsCombineTest.Asymptotic.mH125.6.mR${radion[$j]}_higgs.txt
-                mv higgsCombineTest.Asymptotic.mH120.root higgsCombineTest.Asymptotic.mH125.mR${radion[$j]}_higgs.root
-	        echo did with Higgs
+	    combine -M Asymptotic --run blind hgg.mH125.0_8TeV.txt -S 0 >> higgsCombineTest.Asymptotic.mH125.6.mR${imass}_nosyst_higgs.txt
+	    mv higgsCombineTest.Asymptotic.mH120.root higgsCombineTest.Asymptotic.mH125.mR${imass}_nosyst_higgs.root
+	    echo did with no syst with higgs
+	    rm roostats*
+	    cd ../..
+	else
+	    combine -M Asymptotic hgg.mH125.0_8TeV.txt >> higgsCombineTest.Asymptotic.mH125.6.mR${imass}_higgs.txt
+	    mv higgsCombineTest.Asymptotic.mH120.root higgsCombineTest.Asymptotic.mH125.mR${imass}_higgs.root
+	    echo did with Higgs
 	        
-                combine -M Asymptotic hgg.mH125.0_8TeVonecatnohiggs.txt >> higgsCombineTest.Asymptotic.mH125.6.mR${radion[$j]}_onecatnohiggs.txt
-                mv higgsCombineTest.Asymptotic.mH120.root higgsCombineTest.Asymptotic.mH125.mR${radion[$j]}_onecatnohiggs.root
-	        echo did one categ no higgs
+	    combine -M Asymptotic hgg.mH125.0_8TeVonecatnohiggs.txt >> higgsCombineTest.Asymptotic.mH125.6.mR${imass}_onecatnohiggs.txt
+	    mv higgsCombineTest.Asymptotic.mH120.root higgsCombineTest.Asymptotic.mH125.mR${imass}_onecatnohiggs.root
+	    echo did one categ no higgs
 
-                combine -M Asymptotic hgg.mH125.0_8TeV.txt -S 0 >> higgsCombineTest.Asymptotic.mH125.6.mR${radion[$j]}_nosyst_higgs.txt
-                mv higgsCombineTest.Asymptotic.mH120.root higgsCombineTest.Asymptotic.mH125.mR${radion[$j]}_nosyst_higgs.root
-	        echo did with no syst with higgs
-                rm roostats*
-                cd ../..
-        fi
-  done # mass
-done # wp
+	    combine -M Asymptotic hgg.mH125.0_8TeV.txt -S 0 >> higgsCombineTest.Asymptotic.mH125.6.mR${imass}_nosyst_higgs.txt
+	    mv higgsCombineTest.Asymptotic.mH120.root higgsCombineTest.Asymptotic.mH125.mR${imass}_nosyst_higgs.root
+	    echo did with no syst with higgs
+	    rm roostats*
+	    cd ../..
+	fi
+    done # mass
+done #dirs
+
 
