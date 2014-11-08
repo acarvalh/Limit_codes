f690f17c8f85b091e6773cd85c425c26d4b5c6b7
hebda <phil.hebda@gmail.com>
2014-11-08 01:26:27 +0100

    fixed bugs with sed

--- fits.sh
+++ fits.sh
@@ -7,10 +7,10 @@ basedir="/afs/cern.ch/work/o/obondu/public/forRadion/limitTrees/$version"
 limitdirs=("${version}_fitToMgg_nonresSearch_withKinFit" "${version}_fitToMgg_resSearch_withKinFit" "${version}_fitToMgg_resSearch_withRegKinFit" "${version}_fitTo2D_nonresSearch_withKinFit" "${version}_fitTo2D_resSearch_withRegKinFit" "${version}_fitTo2D_resSearch_withKinFit" "${version}_fitToFTR14001_nonresSearch_withKinFit")
 doResLimits=("0" "1" "1" "0" "1" "1" "0")
 do2DLimits=("0" "0" "0" "1" "1" "1" "1")
-mkdir radlim
 
 #If you only want to run on a subset of directories, edit this array with the appropriate indices.
-runLimts=("0" "1" "2" "3" "4" "5" "6")
+runLimits=("0" "1" "2" "3" "4" "5" "6")
+runLimits=("0")
 
 for i in `echo ${runLimits[@]}`; do
 
@@ -31,28 +31,28 @@ for i in `echo ${runLimits[@]}`; do
     for imass in `echo ${masses[@]}`; do
 
 	#Prepare the input files
-	sed -i "/TString hhiggsggh/c\TString hhiggsggh = \"$dir/${limitdirs[$i]}/ggh_m125_powheg_8TeV_m${imass}.root\"/" $fitterScript
-	sed -i "/TString hhiggstth/c\TString hhiggstth = \"$dir/${limitdirs[$i]}/tth_m125_8TeV_m${imass}.root\"/" $fitterScript
-	sed -i "/TString hhiggsvbf/c\TString hhiggsvbf = \"$dir/${limitdirs[$i]}/vbf_m125_8TeV_m${imass}.root\"/" $fitterScript
-	sed -i "/TString hhiggsvh/c\TString hhiggsvh = \"$dir/${limitdirs[$i]}/wzh_m125_8TeV_zh_m${imass}.root\"/" $fitterScript
-	sed -i "/TString hhiggsbbh/c\TString hhiggsbbh = \"$dir/${limitdirs[$i]}/bbh_m125_8TeV_m${imass}.root\"/" $fitterScript
-	sed -i "/TString ddata/c\TString ddata = \"$dir/${limitdirs[$i]}/Data_m${imass}.root\"/" $fitterScript
+	sed -i "/TString hhiggsggh/c\  TString hhiggsggh = \"$dir/ggh_m125_powheg_8TeV_m${imass}.root\";" $fitterScript
+	sed -i "/TString hhiggstth/c\  TString hhiggstth = \"$dir/tth_m125_8TeV_m${imass}.root\";" $fitterScript
+	sed -i "/TString hhiggsvbf/c\  TString hhiggsvbf = \"$dir/vbf_m125_8TeV_m${imass}.root\";" $fitterScript
+	sed -i "/TString hhiggsvh/c\  TString hhiggsvh = \"$dir/wzh_m125_8TeV_zh_m${imass}.root\";" $fitterScript
+	sed -i "/TString hhiggsbbh/c\  TString hhiggsbbh = \"$dir/bbh_m125_8TeV_m${imass}.root\";" $fitterScript
+	sed -i "/TString ddata/c\  TString ddata = \"$dir/Data_m${imass}.root\";" $fitterScript
 
 	if [ ${imass} == "0" ]; then
-	    sed -i "/TString ssignal/c\TString ssignal = \"$dir/${limitdirs[$i]}/ggHH_8TeV_m0.root\"/" $fitterScript
+	    sed -i "/TString ssignal/c\  TString ssignal = \"$dir/ggHH_8TeV_m0.root\";" $fitterScript
 	elif [ ${imass} == "260" ]; then
-	    sed -i "/TString ssignal/c\TString ssignal = \"$dir/${limitdirs[$i]}/MSSM_m260_8TeV_m260.root\"/" $fitterScript
+	    sed -i "/TString ssignal/c\  TString ssignal = \"$dir/MSSM_m260_8TeV_m260.root\";" $fitterScript
 	else
-	    sed -i "/TString ssignal/c\TString ssignal = \"$dir/${limitdirs[$i]}/Radion_m${imass}_8TeV_${imass}.root\"/" $fitterScript
+	    sed -i "/TString ssignal/c\  TString ssignal = \"$dir/Radion_m${imass}_8TeV_${imass}.root\";" $fitterScript
 	fi
 
         # the legend
 	sed -i -r -e "s/[0-9]+ GeV\"\);/${masses[$j]} GeV\"\);/g" $fitterScript
 
-	echo "Running limits for $imass on ${limitdirs[$i]}"
+	echo "Running limits for mass $imass on ${limitdirs[$i]}"
 
 	outputdir="radlim_${limitdirs[$i]}/radlim${imass}"
-	mkdir $outputdir
+	mkdir -p $outputdir
 	root -l -q runmgg.C >> ${outputdir}/log_radlim${imass}.txt
 	mv workspaces/*.root $outputdir
 	mv datacards/*.txt $outputdir
