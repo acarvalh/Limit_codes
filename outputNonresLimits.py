582b5232cd108da204bd01ab3e47dc2f07f83fb6
hebda <phil.hebda@gmail.com>
2015-01-16 18:53:30 +0100

    more stuff

--- outputNonresLimits.py
+++ outputNonresLimits.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
 
+#run with args <output from combine> <nonres senario> <output file for all limits>
+./outputNonresLimits.py $outputdir/$outputfile $limitOutputFile
+
+import sys
+
+if len(sys.argv) < 4:
+    echo "Not enough arguments"
+    exit 1
+
+    
+combineFile=sys.argv[1]
+scenario=sys.argv[2]
+outputFile=sys.argv[3]
+
+
+
